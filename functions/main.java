0b9e0bb99a3cbf38781c799de2322faf00dd9baa
Kartik Joshi <gopaljoshi8914@gmail.com>
2026-06-12 18:59:52 +0530

    practice some more questions of functions

--- functions/main.java
+++ functions/main.java
@@ -5,7 +5,6 @@ public class main {
 
     public static void printMyName(String name) {
         System.out.println(name);
-        return;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
