ea3ad4cc965ade671af9088f8b5b229e7900cec2
Kartik Joshi <gopaljoshi8914@gmail.com>
2026-06-13 03:01:13 +0530

    practice arrays and 2-D arrays

--- functions/FibonacciSeries.java
+++ functions/FibonacciSeries.java
@@ -1,26 +1,19 @@
-import java.util.Scanner;
-
-public class FibonacciSeries {
-
-    public static void printFibonacci(int n) {
+import java.util.*;
+class FibonacciSeries {
+    
+    public static void printFibonacci(int n){
         int a = 0, b = 1;
-
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++){
             System.out.print(a + " ");
-
+            
             int next = a + b;
             a = b;
             b = next;
         }
     }
-
     public static void main(String[] args) {
-
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.print("Enter number of terms: ");
-            int n = sc.nextInt();
-
-            printFibonacci(n);
-        }
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printFibonacci(n);
     }
 }
