ae53bd23341f4cb5d223dff00ce91b83e8743c0f
Kartik Joshi <gopaljoshi8914@gmail.com>
2026-07-09 23:45:00 +0530

    remove, add and print elements in linked list

--- linkedList/linkedList2.java
+++ linkedList/linkedList2.java
@@ -11,6 +11,28 @@ class linkedList2 {
 
         list.addLast("the");
         list.addLast("ghost");
+        list.add("got it");
+        System.out.println(list);
+
+        System.out.println(list.size());
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + "->");
+        }
+
+        System.out.print("null");
+
+        System.out.println();
+        System.out.println("-----------------------------------------");
+
+        list.removeFirst();
+        System.out.println(list);
+        list.removeLast();
+        System.out.println(list);
+
+        System.out.println("-----------------------------------------");
+
+        list.remove(1);
         System.out.println(list);
     }
 }
